e862f3ddb1e6b5f7dd2ad662b756c70924e71426
dvlpsh <dvlpsh21@gmail.com>
2019-10-26 00:27:22 +0530

    Update

--- test.py
+++ test.py
@@ -1,36 +1,31 @@
 import os
 
 
-def main():
-	print('The current working directory is')
 
-	os.system('pwd')
-	#clone, set up origin, upstream, make changes, push to local origin, finally pull request
+print('The current working directory is')
+os.system('pwd')
+#clone, set up origin, upstream, make changes, push to local origin, finally pull request
+#init and clone
 
-	#init and clone
-	os.system('git init')
-	url=input('Enter url of forked repo: ')
-	clone='git clone '+url
-	os.system(clone)
+os.system('git init')
+url=input('Enter url of forked repo: ')
+clone='git clone '+url
+os.system(clone)
 
-	#set remote origin, your local copy
-	orgadd='git remote add origin '+url
-	os.system(orgadd)
-	os.system('git remote -v')
-	print("======================================================================================================================\n")
+#set remote origin, your local copy
+orgadd='git remote add origin '+url
+os.system(orgadd)
+os.system('git remote -v')
+print("======================================================================================================================\n")
 
-	#set upstream
-	upstadd='git remote add upstream '+ input('Enter url to the original repo: ')
-	os.system(upstadd)
-	os.system('git remote -v')
-	print("======================================================================================================================\n")
+#set upstream
+upstadd='git remote add upstream '+ input('Enter url to the original repo: ')
+os.system(upstadd)
+os.system('git remote -v')
+print("======================================================================================================================\n")
 
-	#branch and make changes
-	branch_name=input('Enter branch-name')
-	branch='git checkout -b '+branch_name
-	os.system(branch)
-	print('Time to make some changes!')
-
-
-if __name__ == "__main__":
-	main()
+#branch and make changes
+branch_name=input('Enter branch-name')
+branch='git checkout -b '+branch_name
+os.system(branch)
+print('Time to make some changes!')
