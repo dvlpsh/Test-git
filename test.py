02f3a1da849e02f471ad6b827441983dec2d2a03
dvlpsh <dvlpsh21@gmail.com>
2019-10-26 00:24:47 +0530

    Update

--- test.py
+++ test.py
@@ -1,26 +1,36 @@
 import os
 
-print('The current working directory is')
 
-os.system('pwd')
-#clone, set up origin, upstream, make changes, push to local origin, finally pull request
+def main():
+	print('The current working directory is')
 
-#init and clone
-os.system('git init')
-url=input('Enter url of forked repo: ')
-clone='git clone '+url
-os.system(clone)
+	os.system('pwd')
+	#clone, set up origin, upstream, make changes, push to local origin, finally pull request
 
-#set remote origin, your local copy
-orgadd='git remote add origin '+url
-os.system(orgadd)
-os.system('git remote -v')
+	#init and clone
+	os.system('git init')
+	url=input('Enter url of forked repo: ')
+	clone='git clone '+url
+	os.system(clone)
 
-#set upstream
-upstadd='git remote add upstream '+ input('Enter url to the original repo: ')
-os.system(upstadd)
-os.system('git remote -v')
+	#set remote origin, your local copy
+	orgadd='git remote add origin '+url
+	os.system(orgadd)
+	os.system('git remote -v')
+	print("======================================================================================================================\n")
 
-print('Time to make some changes!')
+	#set upstream
+	upstadd='git remote add upstream '+ input('Enter url to the original repo: ')
+	os.system(upstadd)
+	os.system('git remote -v')
+	print("======================================================================================================================\n")
 
-#os.system('git remote add origin https://github.com/'+usname+'/'+repo+'.git')
+	#branch and make changes
+	branch_name=input('Enter branch-name')
+	branch='git checkout -b '+branch_name
+	os.system(branch)
+	print('Time to make some changes!')
+
+
+if __name__ == "__main__":
+	main()
